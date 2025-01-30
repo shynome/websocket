93f7fa1055da28bd2f3a93ee969bcc777c9c7023
shynome <shynome@gmail.com>
2025-01-30 21:34:43 +0800

    迁移到 Conn 接口

--- dial.go
+++ dial.go
@@ -118,11 +118,11 @@ func (opts *DialOptions) cloneWithDefaults(ctx context.Context) (context.Context
 // See docs on the HTTPClient option and https://github.com/golang/go/issues/26937#issuecomment-415855861
 //
 // URLs with http/https schemes will work and are interpreted as ws/wss.
-func Dial(ctx context.Context, u string, opts *DialOptions) (*StdConn, *http.Response, error) {
+func Dial(ctx context.Context, u string, opts *DialOptions) (Conn, *http.Response, error) {
 	return dial(ctx, u, opts, nil)
 }
 
-func dial(ctx context.Context, urls string, opts *DialOptions, rand io.Reader) (_ *StdConn, _ *http.Response, err error) {
+func dial(ctx context.Context, urls string, opts *DialOptions, rand io.Reader) (_ Conn, _ *http.Response, err error) {
 	defer errd.Wrap(&err, "failed to WebSocket dial")
 
 	var cancel context.CancelFunc
--- internal/test/wstest/pipe.go
+++ internal/test/wstest/pipe.go
@@ -15,7 +15,7 @@ import (
 
 // Pipe is used to create an in memory connection
 // between two websockets analogous to net.Pipe.
-func Pipe(dialOpts *websocket.DialOptions, acceptOpts *websocket.AcceptOptions) (clientConn, serverConn *websocket.StdConn) {
+func Pipe(dialOpts *websocket.DialOptions, acceptOpts *websocket.AcceptOptions) (clientConn, serverConn websocket.Conn) {
 	tt := fakeTransport{
 		h: func(w http.ResponseWriter, r *http.Request) {
 			serverConn, _ = websocket.Accept(w, r, acceptOpts)
